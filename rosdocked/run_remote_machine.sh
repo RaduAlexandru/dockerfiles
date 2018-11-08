8bf4f57ff3db4a26f35a9ac60668200ea6a6e6b5
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2018-11-08 01:20:38 +0000

    Added back the previous rosdocked which I will now consider deprecated in favour of mini

---
+++ rosdocked/run_remote_machine.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Check args
+if [ "$#" -ne 1 ]; then
+  echo "usage: ./run.sh IMAGE_NAME"
+  return 1
+fi
+
+# Get this script's path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+set -e
+
+#--runtime=nvidia\ # add this in case you use the nvidia one2
+
+# Run the container with shared X11
+docker run\
+  --net=host\
+  --privileged\
+  -e SHELL\
+  -e DISPLAY\
+  -e DOCKER=1\
+  -v "$HOME:$HOME:rw"\
+  -v "/home/local/rosu:/home/local/rosu:rw"\
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+  -it $1 $SHELL
