ddbb457fad2be1e120767ee03fe91f544cb40a57
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-02-22 11:35:46 +0000

    Removed conda things and added some new hosts for mbzirc and bigcuda5

--- mini_18/setup.sh
+++ mini_18/setup.sh
@@ -3,6 +3,12 @@ echo "Performing setup of the system by writing some config files"
 ./echo_to_file.sh ~/.bashrc "source /opt/ros/melodic/setup.bash"
 ./echo_to_file.sh ~/.bashrc "source /media/rosu/Data/phd/c_ws/devel/setup.bash"
 #activate conda environment which is the same in which we install stuff in the Dockerfile
-./echo_to_file.sh ~/.bashrc "source activate pt"
+# ./echo_to_file.sh ~/.bashrc "source activate pt"
 #make it so that conda doesnt modify the terminal line when we activate the conda environment
-./echo_to_file.sh ~/.condarc "changeps1: False"
+# touch ~/.condarc
+# ./echo_to_file.sh ~/.condarc "changeps1: false"
+
+#some nice hosts to have
+./echo_to_file.sh /etc/hosts "10.7.3.52       mbzirc2"
+./echo_to_file.sh /etc/hosts "131.220.7.55       bigcuda5"
+
