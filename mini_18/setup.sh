15dffc1ee5523aa0181e9c11b877abb4e01da770
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2019-06-19 08:57:49 +0000

    More cuda and python tools

--- mini_18/setup.sh
+++ mini_18/setup.sh
@@ -2,6 +2,9 @@ echo "Performing setup of the system by writing some config files"
 #ros sourcing
 ./echo_to_file.sh ~/.bashrc "source /opt/ros/melodic/setup.bash"
 ./echo_to_file.sh ~/.bashrc "source /media/rosu/Data/phd/c_ws/devel/setup.bash"
+
+echo "Setting up Kdenlive by exporting dbus-launch"
+./echo_to_file ~/.bashrc "export $(dbus-launch)"
 #activate conda environment which is the same in which we install stuff in the Dockerfile
 # ./echo_to_file.sh ~/.bashrc "source activate pt"
 #make it so that conda doesnt modify the terminal line when we activate the conda environment
