c82d997b97c3cfd82ab5aa3239e527327e939a32
Radu Alexandru Rosu <rosu@bigcuda5.informatik.uni-bonn.de>
2020-01-24 17:05:23 +0100

    Runs on bigcuda5

--- mini_18_remote/run.sh
+++ mini_18_remote/run.sh
@@ -15,7 +15,6 @@ set -e
 
 # Run the container with shared X11
 docker run\
-  --runtime=nvidia\
   --gpus all\
   --publish-all=true\
   --net=host\
@@ -28,4 +27,5 @@ docker run\
   -v "/media/rosu/INTENSO:/media/rosu/INTENSO:rw"\
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
   -v "/opt/intel:/opt/intel:rw"\
+  -v "/home/local/staff/rosu:/home/local/staff/rosu:rw"\
   -it $1
