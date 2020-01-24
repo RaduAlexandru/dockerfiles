a23579e262340db9f2a23fe394d7caf5f5e89908
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-01-24 09:56:52 +0000

    Added dockerfile for remote

---
+++ mini_18_remote/run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Check args
+if [ "$#" -ne 1 ]; then
+  echo "usage: ./run.sh IMAGE_NAME"
+  return 1
+fi
+
+# Get this script's path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+set -e
+
+# Run the container with shared X11
+docker run\
+  --runtime=nvidia\
+  --gpus all\
+  --publish-all=true\
+  --net=host\
+  --privileged\
+  -e SHELL\
+  -e DISPLAY\
+  -e DOCKER=1\
+  -v "$HOME:$HOME:rw"\
+  -v "/media/rosu/Data:/media/rosu/Data:rw"\
+  -v "/media/rosu/INTENSO:/media/rosu/INTENSO:rw"\
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+  -v "/opt/intel:/opt/intel:rw"\
+  -it $1
