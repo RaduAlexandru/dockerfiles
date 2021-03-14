a932436ba398708a9597be13298f196f101917b4
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2021-03-14 22:12:46 +0000

    Updated Pytorch to 1.7.1 and cuda

--- basic/run.sh
+++ basic/run.sh
@@ -15,15 +15,18 @@ set -e
 
 # Run the container with shared X11
 docker run\
-  --runtime=nvidia\
+  --shm-size 2G\
+  --gpus all\
   --publish-all=true\
   --net=host\
   --privileged\
   -e SHELL\
   -e DISPLAY\
   -e DOCKER=1\
+  -v /dev:/dev\
   -v "$HOME:$HOME:rw"\
   -v "/media/rosu/Data:/media/rosu/Data:rw"\
+  -v "/media/rosu/HDD:/media/rosu/HDD:rw"\
   -v "/media/rosu/INTENSO:/media/rosu/INTENSO:rw"\
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
   -v "/opt/intel:/opt/intel:rw"\
